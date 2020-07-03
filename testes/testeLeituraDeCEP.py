4fb81258c2ad45423b31d8f120e8eb8035ba542c
Syank <rafs.97@hotmail.com>
2020-07-03 14:25:07 -0300

    Atualizações

--- testes/testeLeituraDeCEP.py
+++ testes/testeLeituraDeCEP.py
@@ -1,18 +1,19 @@
-print('Importando libs')
+print('Importando libs...')
 import pandas as pd
 import pycep_correios as pycep
 import os.path
+from matplotlib import pyplot as plt
 
 
 # Váriaveis soltas
 enderecos = []
-cidades = []
+ufs = {}
 
 
 #A linha a baixo encontra o arquivo na pasta em que ele está, indepente do computador
 arquivo = os.path.abspath('').replace('testes', 'Bancos_de_dados\CNES_01_04_2020_banco_estab.xlsx')
 
-print('Lendo xlsx')
+print('Lendo xlsx...')
 ceps = pd.read_excel(arquivo, usecols = ['CO_CEP'])  # Atenção, pode demorar algum tempo para ler tudo
 
 amostras = ceps.sample(10)
@@ -24,11 +25,14 @@ for chave in chaves:
     cep = str(amostras.CO_CEP.get(chave))
     while len(cep) < 8:  # CEPs antigos tem menos digitos, esse loop corrige isso atualizando eles para 8 digitos
         cep = '0' + cep
-    enderecos.append(pycep.get_address_from_cep(cep))
+    ufDoEstabelecimento = pycep.get_address_from_cep(cep)['uf']
+    if ufDoEstabelecimento not in ufs:
+        ufs[ufDoEstabelecimento] = 1
+    else:
+        ufs[ufDoEstabelecimento] += 1
 
 
 # O loop a baixo pega as cidades de cada endereço e as guarda
-for endereco in enderecos:
-    cidades.append(endereco['cidade'])
 
-print(cidades)
+
+print(ufs)
